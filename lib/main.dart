5abf1d0e5f3e73fd3b0245772598a10260b041ea
jted0537 <jted0537@gmail.com>
2021-04-07 03:21:52 +0900

    fixing problems..

--- lib/main.dart
+++ lib/main.dart
@@ -18,49 +18,9 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MapSample extends StatefulWidget {
-  @override
-  State<MapSample> createState() => MapSampleState();
-}
-
-class MapSampleState extends State<MapSample> {
-  Completer<GoogleMapController> _controller = Completer();
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(1, 1),
-    zoom: 14.4746,
-  );
-
-  static final CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      body: GoogleMap(
-        mapType: MapType.hybrid,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: Text('To the lake!'),
-        icon: Icon(Icons.directions_boat),
-      ),
-    );
-  }
-
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
-  }
-}
-
 class LoadingScreen extends StatefulWidget {
+  //LoadingScreen({Key key, this.title}) : super(key: key);
+
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
@@ -68,10 +28,15 @@ class LoadingScreen extends StatefulWidget {
 class _LoadingScreenState extends State<LoadingScreen> {
   Position _currentPosition;
   Completer<GoogleMapController> _controller = Completer();
+  BitmapDescriptor pinLocationIcon;
 
   void initState() {
     super.initState();
-    _determinePosition();
+    BitmapDescriptor.fromAssetImage(
+            ImageConfiguration(size: Size(5, 5)), "images/marker3.png")
+        .then((onValue) {
+      pinLocationIcon = onValue;
+    });
   }
 
   @override
@@ -81,11 +46,23 @@ class _LoadingScreenState extends State<LoadingScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            _buildGoogleMap(
-                context, _currentPosition.latitude, _currentPosition.longitude),
             if (_currentPosition != null)
-              Text(
-                  "위도: ${_currentPosition.latitude}, 경도: ${_currentPosition.longitude}"),
+              // SizedBox(
+              //     width: MediaQuery.of(context).size.width - 10,
+              //     height: MediaQuery.of(context).size.height / 2,
+              //     child: GoogleMap(
+              //       mapType: MapType.normal,
+              //       initialCameraPosition: CameraPosition(
+              //         target: LatLng(_currentPosition.latitude,
+              //             _currentPosition.longitude),
+              //         zoom: 15,
+              //       ),
+              //       markers: _createMarker(
+              //           _currentPosition.latitude, _currentPosition.longitude),
+              //     )),
+              if (_currentPosition != null)
+                Text(
+                    "위도: ${_currentPosition.latitude}, 경도: ${_currentPosition.longitude}"),
             FlatButton(
               child: Text("Get location"),
               onPressed: () {
@@ -98,6 +75,32 @@ class _LoadingScreenState extends State<LoadingScreen> {
     );
   }
 
+  _getCurrentLocation() {
+    Geolocator.getCurrentPosition(
+            desiredAccuracy: LocationAccuracy.best,
+            forceAndroidLocationManager: true)
+        .then((Position position) {
+      setState(() {
+        _currentPosition = position;
+      });
+    }).catchError((e) {
+      print(e);
+    });
+  }
+
+  Set<Marker> _createMarker(double latitude, double longitude) {
+    return <Marker>[
+      Marker(
+        markerId: MarkerId("marker_1"),
+        position: LatLng(latitude, longitude),
+        icon: pinLocationIcon,
+        infoWindow: InfoWindow(
+          title: "d",
+        ),
+      ),
+    ].toSet();
+  }
+
   // Get User location after check service enablement and permission
   Future<void> _determinePosition() async {
     bool serviceEnabled;
@@ -107,9 +110,6 @@ class _LoadingScreenState extends State<LoadingScreen> {
     serviceEnabled = await Geolocator.isLocationServiceEnabled();
     print("service : " + serviceEnabled.toString());
     if (!serviceEnabled) {
-      // Location services are not enabled don't continue
-      // accessing the position and request users of the
-      // App to enable the location services.
       return Future.error('Location services are disabled.');
     }
 
@@ -119,23 +119,15 @@ class _LoadingScreenState extends State<LoadingScreen> {
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
       if (permission == LocationPermission.deniedForever) {
-        // Permissions are denied forever, handle appropriately.
         return Future.error(
             'Location permissions are permanently denied, we cannot request permissions.');
       }
 
       if (permission == LocationPermission.denied) {
-        // Permissions are denied, next time you could try
-        // requesting permissions again (this is also where
-        // Android's shouldShowRequestPermissionRationale
-        // returned true. According to Android guidelines
-        // your App should show an explanatory UI now.
         return Future.error('Location permissions are denied');
       }
     }
 
-    // When we reach here, permissions are granted and we can
-    // continue accessing the position of the device.
     Geolocator.getCurrentPosition(
             desiredAccuracy: LocationAccuracy.best,
             forceAndroidLocationManager: true)
@@ -150,15 +142,16 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 }
 
-Widget _buildGoogleMap(
-        BuildContext context, double latitude, double longitude) =>
-    SizedBox(
-        width: MediaQuery.of(context).size.width - 10,
-        height: MediaQuery.of(context).size.height / 2,
-        child: GoogleMap(
-          mapType: MapType.normal,
-          initialCameraPosition: CameraPosition(
-            target: LatLng(latitude, longitude),
-            zoom: 15,
-          ),
-        ));
+// Widget _buildGoogleMap(
+//         BuildContext context, double latitude, double longitude) =>
+//     SizedBox(
+//         width: MediaQuery.of(context).size.width - 10,
+//         height: MediaQuery.of(context).size.height / 2,
+//         child: GoogleMap(
+//           mapType: MapType.normal,
+//           initialCameraPosition: CameraPosition(
+//             target: LatLng(latitude, longitude),
+//             zoom: 15,
+//           ),
+//           markers: _createMarker(latitude, longitude),
+//         ));
