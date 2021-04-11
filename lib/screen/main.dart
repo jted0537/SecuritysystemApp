e90ab9ed6b9c9f4414bd48ba3a42d4457349f5f0
jted0537 <jted0537@gmail.com>
2021-04-12 03:31:27 +0900

    Realtime tracking with fetch BE

--- lib/screen/main.dart
+++ lib/screen/main.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -6,8 +8,7 @@ import 'package:security_system/viewmodels/check_points_view_model.dart';
 import 'dart:async';
 
 void main() async {
-  runApp(MaterialApp(home: Scaffold(body: LoadingScreen())));
-
+  runApp(MaterialApp(home: LoadingScreen()));
   //runApp(MyApp());
 }
 
@@ -21,157 +22,152 @@ class _LoadingScreenState extends State<LoadingScreen> {
   List<dynamic> checkpointsjson = [];
   final CheckPointViewModel viewModel = CheckPointViewModel();
 
-  BitmapDescriptor pinLocationIcon;
-  Position _currentPosition;
-  Completer<GoogleMapController> _controller = Completer();
+  GoogleMapController _controller;
+  StreamSubscription _positionSubscription;
+  Marker marker;
+  Circle circle;
 
   @override
   void initState() {
     super.initState();
-    BitmapDescriptor.fromAssetImage(
-            ImageConfiguration(size: Size(5, 5)), "images/marker3.png")
-        .then((onValue) {
-      pinLocationIcon = onValue;
-    });
     _requestCheckPoints();
   }
 
-  _requestCheckPoints() async {
+  Future<Uint8List> getMarker() async {
+    ByteData byteData =
+        await DefaultAssetBundle.of(context).load("images/marker3.png");
+    return byteData.buffer.asUint8List();
+  }
+
+  void getCurrentLocation() async {
+    try {
+      Uint8List imageData = await getMarker();
+      var currPosition = await Geolocator.getCurrentPosition();
+
+      updateMarkerAndCircle(currPosition, imageData);
+
+      if (_positionSubscription != null) {
+        _positionSubscription.cancel();
+      }
+
+      _positionSubscription =
+          Geolocator.getPositionStream(desiredAccuracy: LocationAccuracy.best)
+              .listen((Position newPosition) {
+        if (_controller != null) {
+          _controller
+              .animateCamera(CameraUpdate.newCameraPosition(new CameraPosition(
+                  //bearing: 192.8334901395799,
+                  target: LatLng(newPosition.latitude, newPosition.longitude),
+                  //tilt: 0,
+                  zoom: 18.00)));
+          updateMarkerAndCircle(newPosition, imageData);
+          print(newPosition.latitude.toString() +
+              newPosition.longitude.toString());
+        }
+      });
+    } on PlatformException catch (e) {
+      if (e.code == 'PERMISSION_DENIED') {
+        debugPrint("Permission Denied");
+      }
+    }
+  }
+
+  void stopTracking() {
+    _positionSubscription.pause();
+  }
+
+  void dispose() {
+    if (_positionSubscription != null) {
+      _positionSubscription.cancel();
+    }
+    super.dispose();
+  }
+
+  void updateMarkerAndCircle(Position newLocalData, Uint8List imageData) {
+    LatLng latlng = LatLng(newLocalData.latitude, newLocalData.longitude);
+    this.setState(() {
+      marker = Marker(
+          markerId: MarkerId("home"),
+          position: latlng,
+          //rotation: newLocalData.,
+          draggable: false,
+          zIndex: 2,
+          flat: true,
+          anchor: Offset(0.5, 0.5),
+          icon: BitmapDescriptor.fromBytes(imageData));
+      circle = Circle(
+          circleId: CircleId("car"),
+          radius: newLocalData.accuracy,
+          zIndex: 1,
+          strokeColor: Colors.blue,
+          center: latlng,
+          fillColor: Colors.blue.withAlpha(70));
+    });
+  }
+
+  void _requestCheckPoints() async {
     var tmp = await viewModel.fetchCheckPoint();
-    print(tmp);
     setState(() {
       checkpointsjson = tmp;
     });
     checkpoints = checkpointsjson
         .map((checkpoint) => CheckPoint.fromJson(checkpoint))
         .toList();
-    print(checkpoints[0].checkPointId);
+    print(checkpoints);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      //scrollDirection: Axis.vertical,
-      //shrinkWrap: true,
-      children: [
-        //Image.asset('images/CVGM-Azonix.png', height: 150, width: 150),
-        //ListWidget(),
-        // if (_currentPosition != null)
-        //   SizedBox(
-        //       width: MediaQuery.of(context).size.width / 2,
-        //       height: MediaQuery.of(context).size.height / 4,
-        //       child: GoogleMap(
-        //         mapType: MapType.normal,
-        //         initialCameraPosition: CameraPosition(
-        //           target: LatLng(
-        //               _currentPosition.latitude, _currentPosition.longitude),
-        //           zoom: 15,
-        //         ),
-        //         // markers: _createMarker(
-        //         //     _currentPosition.latitude, _currentPosition.longitude),
-        //       )),
-        if (_currentPosition != null)
-          Text(
-              "위도: ${_currentPosition.latitude}, 경도: ${_currentPosition.longitude}"),
-        FlatButton(
-          child: Text("Get location"),
-          onPressed: () async {
-            await Geolocator.getCurrentPosition()
-                .then((position) => _currentPosition = position);
-            setState(() {});
-          },
-        ),
-      ],
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('images/CVGM-Azonix.png', height: 150, width: 150),
+          SizedBox(
+              width: MediaQuery.of(context).size.width - 30,
+              height: MediaQuery.of(context).size.height / 2,
+              child: GoogleMap(
+                mapType: MapType.normal,
+                initialCameraPosition: CameraPosition(
+                  target: LatLng(37.5668919, 126.9179338),
+                  zoom: 15,
+                ),
+                markers: Set.of((marker != null) ? [marker] : []),
+                circles: Set.of((circle != null) ? [circle] : []),
+                onMapCreated: (GoogleMapController controller) {
+                  _controller = controller;
+                },
+              )),
+          FlatButton(
+            child: Text("Stop tracking", style: TextStyle(color: Colors.red)),
+            onPressed: () {
+              stopTracking();
+            },
+          ),
+          ListView.separated(
+            scrollDirection: Axis.vertical,
+            shrinkWrap: true,
+            itemCount: checkpoints.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                  height: 20,
+                  child: Center(
+                    child: Text("CheckPoint " +
+                        index.toString() +
+                        " : Latitude: ${checkpoints[index].latitude}, Longitude: ${checkpoints[index].longitude}"),
+                  ));
+            },
+            separatorBuilder: (BuildContext context, int index) {
+              return Divider();
+            },
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          getCurrentLocation();
+        },
+      ),
     );
   }
 }
-
-// class ListWidget extends StatelessWidget {
-//   final CheckPointViewModel viewModel = CheckPointViewModel();
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return FutureBuilder<List<CheckPoint>>(
-//         future: this.viewModel.fetchCheckPoint(),
-//         builder: (context, snapshot) {
-//           if (snapshot.hasData) {
-//             return ListView.builder(
-//               itemCount: snapshot.data.length,
-//               itemBuilder: (context, index) {
-//                 return ListTile(
-//                   title: Text(snapshot.data[index].checkPointId),
-//                 );
-//               },
-//             );
-//           } else if (snapshot.hasError) {
-//             return Text(snapshot.error);
-//           }
-//           return CircularProgressIndicator();
-//         });
-//   }
-// }
-
-// class MapWidget extends State<LoadingScreen> {
-//   BitmapDescriptor pinLocationIcon;
-//   Position _currentPosition;
-//   Completer<GoogleMapController> _controller = Completer();
-
-//   void initState() {
-//     BitmapDescriptor.fromAssetImage(
-//             ImageConfiguration(size: Size(5, 5)), "images/marker3.png")
-//         .then((onValue) {
-//       pinLocationIcon = onValue;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             if (_currentPosition != null)
-//               SizedBox(
-//                   width: MediaQuery.of(context).size.width - 20,
-//                   height: MediaQuery.of(context).size.height / 2,
-//                   child: GoogleMap(
-//                     mapType: MapType.normal,
-//                     initialCameraPosition: CameraPosition(
-//                       target: LatLng(_currentPosition.latitude,
-//                           _currentPosition.longitude),
-//                       zoom: 15,
-//                     ),
-//                     // markers: _createMarker(
-//                     //     _currentPosition.latitude, _currentPosition.longitude),
-//                   )),
-//             if (_currentPosition != null)
-//               Text(
-//                   "위도: ${_currentPosition.latitude}, 경도: ${_currentPosition.longitude}"),
-//             FlatButton(
-//               child: Text("Get location"),
-//               onPressed: () async {
-//                 await Geolocator.getCurrentPosition()
-//                     .then((position) => _currentPosition = position);
-//                 setState(() {});
-//               },
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-// // Set<Marker> _createMarker(double latitude, double longitude) {
-// //   return <Marker>[
-// //     Marker(
-// //       markerId: MarkerId("marker_1"),
-// //       position: LatLng(latitude, longitude),
-// //       icon: pinLocation,
-// //       infoWindow: InfoWindow(
-// //         title: "d",
-// //       ),
-// //     ),
-// //   ].toSet();
-// // }
--- lib/viewmodels/check_points_view_model.dart
+++ lib/viewmodels/check_points_view_model.dart
@@ -4,7 +4,7 @@ import 'dart:async';
 import 'dart:convert';
 
 class CheckPointViewModel {
-  String url = "https://api.jsonbin.io/b/6072eb580ed6f819bea8e4ac/3";
+  String url = "https://api.jsonbin.io/b/6072eb580ed6f819bea8e4ac/4";
 
   Future<List<dynamic>> fetchCheckPoint() async {
     final response = await http.get(Uri.parse(this.url));
