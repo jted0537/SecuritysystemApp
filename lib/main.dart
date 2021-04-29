25c7be9b29290be1fca08ecb25e44b8b1fca1106
jted0537 <jted0537@gmail.com>
2021-04-30 00:53:41 +0900

    Loign Page Update

---
+++ lib/main.dart
@@ -0,0 +1,302 @@
+import 'package:flutter/services.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(SecureApp());
+
+class SecureApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
+  }
+}
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 200.0, bottom: 200.0),
+              child: Center(
+                child: Container(
+                    width: 200,
+                    height: 150,
+                    child: Image.asset('images/CVGM-Azonix.png',
+                        height: 150, width: 150)),
+              ),
+            ),
+            Padding(
+                padding: const EdgeInsets.only(
+                    left: 15.0, right: 15.0, top: 10, bottom: 0),
+                child: Column(
+                  children: [
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(' Employee ID',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          )),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    TextField(
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: '0172345678',
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(12.0)),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10.0)),
+                            borderSide:
+                                BorderSide(color: Colors.black, width: 2),
+                          ),
+                        )),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        ' Enter Phone Number',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    TextField(
+                        keyboardType: TextInputType.phone,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: '0172345678',
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(12.0)),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10.0)),
+                            borderSide:
+                                BorderSide(color: Colors.black, width: 2),
+                          ),
+                        )),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      width: double.infinity,
+                      height: 50.0,
+                      child: OutlinedButton(
+                        style: OutlinedButton.styleFrom(
+                          primary: Colors.black,
+                          backgroundColor: Colors.grey,
+                        ),
+                        child: Text('NEXT'),
+                        onPressed: () {
+                          print('2222');
+                        },
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      width: double.infinity,
+                      height: 50.0,
+                      child: OutlinedButton(
+                        style: OutlinedButton.styleFrom(
+                          primary: Colors.black,
+                          backgroundColor: Colors.grey,
+                        ),
+                        child: Text('EXIT'),
+                        onPressed: () {
+                          SystemNavigator.pop();
+                        },
+                      ),
+                    ),
+                  ],
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// class LoadingScreen extends StatefulWidget {
+//   @override
+//   _LoadingScreenState createState() => _LoadingScreenState();
+// }
+
+// class _LoadingScreenState extends State<LoadingScreen> {
+//   List<CheckPoint> checkpoints = [];
+//   List<dynamic> checkpointsjson = [];
+//   final CheckPointViewModel viewModel = CheckPointViewModel();
+
+//   GoogleMapController _controller;
+//   StreamSubscription _positionSubscription;
+//   Marker marker;
+//   Circle circle;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _requestCheckPoints();
+//   }
+
+//   Future<Uint8List> getMarker() async {
+//     ByteData byteData =
+//         await DefaultAssetBundle.of(context).load("images/marker3.png");
+//     return byteData.buffer.asUint8List();
+//   }
+
+//   void getCurrentLocation() async {
+//     try {
+//       Uint8List imageData = await getMarker();
+//       var currPosition = await Geolocator.getCurrentPosition();
+
+//       updateMarkerAndCircle(currPosition, imageData);
+
+//       if (_positionSubscription != null) {
+//         _positionSubscription.cancel();
+//       }
+
+//       _positionSubscription =
+//           Geolocator.getPositionStream(desiredAccuracy: LocationAccuracy.best)
+//               .listen((Position newPosition) {
+//         if (_controller != null) {
+//           _controller
+//               .animateCamera(CameraUpdate.newCameraPosition(new CameraPosition(
+//                   //bearing: 192.8334901395799,
+//                   target: LatLng(newPosition.latitude, newPosition.longitude),
+//                   //tilt: 0,
+//                   zoom: 18.00)));
+//           updateMarkerAndCircle(newPosition, imageData);
+//           print(newPosition.latitude.toString() +
+//               newPosition.longitude.toString());
+//         }
+//       });
+//     } on PlatformException catch (e) {
+//       if (e.code == 'PERMISSION_DENIED') {
+//         debugPrint("Permission Denied");
+//       }
+//     }
+//   }
+
+//   void stopTracking() {
+//     _positionSubscription.pause();
+//   }
+
+//   void dispose() {
+//     if (_positionSubscription != null) {
+//       _positionSubscription.cancel();
+//     }
+//     super.dispose();
+//   }
+
+//   void updateMarkerAndCircle(Position newLocalData, Uint8List imageData) {
+//     LatLng latlng = LatLng(newLocalData.latitude, newLocalData.longitude);
+//     this.setState(() {
+//       marker = Marker(
+//           markerId: MarkerId("home"),
+//           position: latlng,
+//           //rotation: newLocalData.,
+//           draggable: false,
+//           zIndex: 2,
+//           flat: true,
+//           anchor: Offset(0.5, 0.5),
+//           icon: BitmapDescriptor.fromBytes(imageData));
+//       circle = Circle(
+//           circleId: CircleId("car"),
+//           radius: newLocalData.accuracy,
+//           zIndex: 1,
+//           strokeColor: Colors.blue,
+//           center: latlng,
+//           fillColor: Colors.blue.withAlpha(70));
+//     });
+//   }
+
+//   void _requestCheckPoints() async {
+//     var tmp = await viewModel.fetchCheckPoint();
+//     setState(() {
+//       checkpointsjson = tmp;
+//     });
+//     checkpoints = checkpointsjson
+//         .map((checkpoint) => CheckPoint.fromJson(checkpoint))
+//         .toList();
+//     print(checkpoints);
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Column(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         children: [
+//           Image.asset('images/CVGM-Azonix.png', height: 150, width: 150),
+//           SizedBox(
+//               width: MediaQuery.of(context).size.width - 30,
+//               height: MediaQuery.of(context).size.height / 2,
+//               child: GoogleMap(
+//                 mapType: MapType.normal,
+//                 initialCameraPosition: CameraPosition(
+//                   target: LatLng(37.5668919, 126.9179338),
+//                   zoom: 15,
+//                 ),
+//                 markers: Set.of((marker != null) ? [marker] : []),
+//                 circles: Set.of((circle != null) ? [circle] : []),
+//                 onMapCreated: (GoogleMapController controller) {
+//                   _controller = controller;
+//                 },
+//               )),
+//           FlatButton(
+//             child: Text("Stop tracking", style: TextStyle(color: Colors.red)),
+//             onPressed: () {
+//               stopTracking();
+//             },
+//           ),
+//           ListView.separated(
+//             scrollDirection: Axis.vertical,
+//             shrinkWrap: true,
+//             itemCount: checkpoints.length,
+//             itemBuilder: (BuildContext context, int index) {
+//               return Container(
+//                   height: 20,
+//                   child: Center(
+//                     child: Text("CheckPoint " +
+//                         index.toString() +
+//                         " : Latitude: ${checkpoints[index].latitude}, Longitude: ${checkpoints[index].longitude}"),
+//                   ));
+//             },
+//             separatorBuilder: (BuildContext context, int index) {
+//               return Divider();
+//             },
+//           )
+//         ],
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () {
+//           getCurrentLocation();
+//         },
+//       ),
+//     );
+//   }
+// }
--- lib/viewmodels/check_points_view_model.dart
+++ lib/viewmodels/check_points_view_model.dart
@@ -1,19 +1,19 @@
-import 'package:security_system/model/chckpoint.dart';
-import 'package:http/http.dart' as http;
-import 'dart:async';
-import 'dart:convert';
+// import 'package:security_system/model/chckpoint.dart';
+// import 'package:http/http.dart' as http;
+// import 'dart:async';
+// import 'dart:convert';
 
-class CheckPointViewModel {
-  String url = "https://api.jsonbin.io/b/6072eb580ed6f819bea8e4ac/4";
+// class CheckPointViewModel {
+//   String url = "https://api.jsonbin.io/b/6072eb580ed6f819bea8e4ac/4";
 
-  Future<List<dynamic>> fetchCheckPoint() async {
-    final response = await http.get(Uri.parse(this.url));
-    print(response.statusCode);
-    if (response.statusCode == 200) {
-      String responseBody = utf8.decode(response.bodyBytes);
-      return jsonDecode(responseBody) as List;
-    } else {
-      throw Exception("failed");
-    }
-  }
-}
+//   Future<List<dynamic>> fetchCheckPoint() async {
+//     final response = await http.get(Uri.parse(this.url));
+//     print(response.statusCode);
+//     if (response.statusCode == 200) {
+//       String responseBody = utf8.decode(response.bodyBytes);
+//       return jsonDecode(responseBody) as List;
+//     } else {
+//       throw Exception("failed");
+//     }
+//   }
+// }
